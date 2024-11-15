de17d6bb988c813870f2b571ecbbfc6c844f45f3
enricoroselino <enricoroselino@gmail.com>
2024-11-15 11:51:46 +0700

    remove guid from LoggingBehavior.cs

--- Shared/Behaviors/LoggingBehavior.cs
+++ Shared/Behaviors/LoggingBehavior.cs
@@ -17,20 +17,18 @@ internal class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
         CancellationToken cancellationToken)
     {
-        var requestId = $"({Guid.NewGuid().ToString()})";
-        _logger.LogInformation("[START] Handling request: {@Request}; Request Data: {@RequestData} -- {@RequestId}",
-            request.GetType().Name, request, requestId);
+        _logger.LogInformation("[START] Handling request: {@Request}; Request Data: {@RequestData}",
+            request.GetType().Name, request);
 
         var startTime = Stopwatch.GetTimestamp();
         var response = await next();
         var deltaSpan = Stopwatch.GetElapsedTime(startTime);
 
         if (deltaSpan.Seconds > 3)
-            _logger.LogWarning("[PERF] Request took {@TimeTaken} seconds -- {@RequestId}", deltaSpan.Seconds,
-                requestId);
+            _logger.LogWarning("[PERF] Request took {@TimeTaken} seconds", deltaSpan.Seconds);
 
-        _logger.LogInformation("[END] Handled request: {@Request}; Execution Time: {@ElapsedTime}ms -- {@RequestId}",
-            request.GetType().Name, deltaSpan.TotalMilliseconds, requestId);
+        _logger.LogInformation("[END] Handled request: {@Request}; Execution Time: {@ElapsedTime}ms",
+            request.GetType().Name, deltaSpan.TotalMilliseconds);
         return response;
     }
 }
