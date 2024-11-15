d6de0db97604bcd689e6ffb30a72813f2e920eff
enricoroselino <enricoroselino@gmail.com>
2024-11-15 11:50:07 +0700

    .

--- Shared/Configurations/SerilogConfiguration.cs
+++ Shared/Configurations/SerilogConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Serilog.Events;
+using Serilog.Filters;
 
 namespace Shared.Configurations;
 
@@ -8,12 +9,22 @@ internal static class SerilogConfiguration
 {
     public static IServiceCollection AddSerilogConfig(this IServiceCollection services)
     {
-        var loggerConfiguration = new LoggerConfiguration()
+        var loggerConfiguration = new LoggerConfiguration();
+        
+        loggerConfiguration
+            .Enrich.FromLogContext()
+            .Enrich.WithEnvironmentName()
+            .Enrich.WithMachineName()
+            .Enrich.WithCorrelationId();
+        
+        loggerConfiguration
             .MinimumLevel.Information()
             .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-            .Enrich.FromLogContext()
-            .WriteTo.Console();
-
+            .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+            .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Error)
+            .Filter.ByExcluding(Matching.FromSource("Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware"));
+        
+        loggerConfiguration.WriteTo.Console();
         Log.Logger = loggerConfiguration.CreateLogger();
         services.AddSerilog(Log.Logger);
         return services;
